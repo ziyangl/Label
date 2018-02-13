8c1fa50186c19a6ac0c5794d576601e659a9dfd8
Ziyang Lan <lzyang0@gmail.com>
2018-02-12 23:19:09 -0600

    no message

--- Label/ViewController.swift
+++ Label/ViewController.swift
@@ -9,10 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var TheLabel: UILabel!
+    
+    var count = 0
+    
+    @IBAction func buttonconnected(_ sender: Any) {
+        count = count + 1
+        
+        if count >= 10{
+            TheLabel.text = "You have clicked the button more than 10 times"
+        } else {
+            TheLabel.text = "click times"
+        }
+    }
+    
+    
+    @IBAction func buttonLeave(_ sender: Any) {
+         self.TheLabel.text = "Good bye!"
+        count = 0
+        print("Button tapped", count)
+        count = 1
+        
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
